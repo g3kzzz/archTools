46dc38cc97a4496cc1ac168e6a42efa490052f01
Fabian Huamani <fabian.huamani@estudiante.ucsm.edu.pe>
2025-08-16 22:08:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -277,7 +277,16 @@ process_files() {
 
 clone_repo "$SECLISTS_REPO" "$USR_SHARE/SecLists"
 clone_repo "$WORDLISTS_REPO" "$WORDLISTS_DIR"
-
+sudo /tools/windows/install_windows_tools.sh
+sudo /tools/linux/install_linux_tools.sh
+sudo rm /tools/windows/install_windows_tools.sh
+sudo rm /tools/linux/install_linux_tools.sh
+# CLONAR
+clone_repo "https://github.com/g333k/tools" "/"
+if ! grep -q 'export PATH=.*\/tools\/bin' "$HOME/.zshrc"; then
+    echo "[*] Agregando /tools/bin al PATH en .zshrc..."
+    echo 'export PATH=$PATH:/tools/bin' >> "$HOME/.zshrc"
+fi
 # =============================
 # FIN
 # =============================
