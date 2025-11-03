de9871045d4c854af91b2467d09744ff2e6c14c4
g3kzzz <fabian.huamani@ucsm.edu.pe>
2025-11-02 19:35:24 -0500

    Actualizacion

--- modules/2.gathering.sh
+++ modules/2.gathering.sh
@@ -6,7 +6,6 @@ set -euo pipefail
 echo "[*] Installing gathering tools..."
 
 sudo pacman -S --needed --noconfirm nmap whois tree dnsutils traceroute curl wget git unzip >/dev/null 2>&1 || true
-yay -S --needed --noconfirm python39 >/dev/null 2>&1 || true
 # AUR tools (i yay exists)
 if command -v yay >/dev/null 2>&1; then
     yay -S --needed --noconfirm theharvester amass subfinder assetfinder httprobe masscan >/dev/null 2>&1 || true
